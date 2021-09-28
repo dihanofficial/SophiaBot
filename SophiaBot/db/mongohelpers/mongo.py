9aa1da4b730c5babf484b73aca89f7ead8386f8b
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-28 10:26:59 +0530

    Update mongo.py

--- SophiaBot/db/mongohelpers/mongo.py
+++ SophiaBot/db/mongohelpers/mongo.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio
 from Sophia import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from Sophia.conf import get_int_key, get_str_key
+from SophiaBot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -17,7 +17,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["Sophia"]
+db = client["SophiaBot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
