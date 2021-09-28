e831740b486ef51c8ee71945213aa810d4d92ae6
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-28 10:28:16 +0530

    Update mongo2.py

--- SophiaBot/db/mongohelpers/mongo2.py
+++ SophiaBot/db/mongohelpers/mongo2.py
@@ -2,9 +2,9 @@
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-from Sophia import MONGO_DB_URI
+from SophiaBot import MONGO_DB_URI
 
 MONGO = MONGO_DB_URI
 
 mongo_client = MongoClient(MONGO)
-db = mongo_client.Sophia
+db = mongo_client.SophiaBot
