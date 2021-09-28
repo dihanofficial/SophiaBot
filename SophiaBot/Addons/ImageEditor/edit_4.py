0dd3ec84df85bdf1b9e2485e6044aaca2b69a002
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-28 10:07:43 +0530

    Update edit_4.py

--- SophiaBot/Addons/ImageEditor/edit_4.py
+++ SophiaBot/Addons/ImageEditor/edit_4.py
@@ -8,7 +8,7 @@ import numpy as np
 import requests
 from PIL import Image, ImageDraw, ImageOps
 
-from Sophia.conf import get_str_key
+from SophiaBot.conf import get_str_key
 
 RemoveBG_API = get_str_key("REM_BG_API_KEY", required=False)
 
