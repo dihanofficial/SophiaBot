98b4bfa884560a8dd78ccef4949d7021e9c767f8
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 07:01:23 +0530

    Update extraction.py

--- SophiaBot/modules/helper_funcs/extraction.py
+++ SophiaBot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Sophia import LOGGER
-from Sophia.modules.users import get_user_id
+from SophiaBot import LOGGER
+from SophiaBot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
