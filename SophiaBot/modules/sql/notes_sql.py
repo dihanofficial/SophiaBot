cbb0e27aee4945fdb45ad52e126afe2574ad6b55
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:40:50 +0530

    Update notes_sql.py

--- SophiaBot/modules/sql/notes_sql.py
+++ SophiaBot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from Sophia.modules.helper_funcs.msg_types import Types
-from Sophia.modules.sql import BASE, SESSION
+from SophiaBot.modules.helper_funcs.msg_types import Types
+from SophiaBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
