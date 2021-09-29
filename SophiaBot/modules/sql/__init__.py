61b3228af47ccb768cc8d35764d44e27a0a786a8
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 07:05:37 +0530

    Update __init__.py

--- SophiaBot/modules/sql/__init__.py
+++ SophiaBot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Sophia import DB_URI
+from SophiaBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
