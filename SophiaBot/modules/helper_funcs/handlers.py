eab1972682e9dd721fc5d8d020a7db387ccdd24a
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 07:02:27 +0530

    Update handlers.py

--- SophiaBot/modules/helper_funcs/handlers.py
+++ SophiaBot/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import Sophia.modules.sql.blacklistusers_sql as sql
-from Sophia import ALLOW_EXCL
-from Sophia import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import SophiaBot.modules.sql.blacklistusers_sql as sql
+from SophiaBot import ALLOW_EXCL
+from SophiaBot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
