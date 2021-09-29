667bfeb86ee7113579708d83ad4bd439d2cc3315
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 06:45:15 +0530

    Update pluginshelper.py

--- SophiaBot/pyrogramee/pluginshelper.py
+++ SophiaBot/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from Sophia  import OWNER_ID, SUPPORT_CHAT
-from Sophia import pbot
+from SophiaBot  import OWNER_ID, SUPPORT_CHAT
+from SophiaBot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
