213104a5f280bd436c6aae1ead098ce1e5bea361
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:42:26 +0530

    Update reporting_sql.py

--- SophiaBot/modules/sql/reporting_sql.py
+++ SophiaBot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from Sophia.modules.sql import BASE, SESSION
+from SophiaBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
