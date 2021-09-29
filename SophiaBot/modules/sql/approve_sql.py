0ad639cea6d66dbd87c5852b695a869f303fd4fd
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:14:53 +0530

    Update approve_sql.py

--- SophiaBot/modules/sql/approve_sql.py
+++ SophiaBot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from Sophia.modules.sql import BASE, SESSION
+from SophiaBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
