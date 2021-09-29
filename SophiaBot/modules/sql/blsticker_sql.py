04cbe3f1834a5314b652e12e92cc61ae0f0ca04b
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:21:23 +0530

    Update blsticker_sql.py

--- SophiaBot/modules/sql/blsticker_sql.py
+++ SophiaBot/modules/sql/blsticker_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from Sophia.modules.sql import BASE, SESSION
+from SophiaBot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, Integer, String, UnicodeText, distinct, func
 
 
