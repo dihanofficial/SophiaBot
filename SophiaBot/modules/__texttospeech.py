abff81957237f39229f751461d7b21bb5f238277
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 17:01:53 +0530

    Update __texttospeech.py

--- SophiaBot/modules/__texttospeech.py
+++ SophiaBot/modules/__texttospeech.py
@@ -1,4 +1,22 @@
-from Sophia import telethn as tbot
+# Copyright (C) 2021 dihan official
+
+# This file is part of Sophia (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from SophiaBot import telethn as tbot
 import os
 
 from gtts import gTTS
@@ -8,9 +26,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from Sophia import *
+from SophiaBot import *
 
-from Sophia.events import register
+from SophiaBot.events import register
 
 
 async def is_register_admin(chat, user):
