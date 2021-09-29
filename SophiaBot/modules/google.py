5d2a46fb8641d19b512e3b0cfd6b707b11f8c2c2
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 16:43:58 +0530

    Update and rename __google.py to google.py

--- SophiaBot/modules/google.py
+++ SophiaBot/modules/google.py
@@ -1,6 +1,24 @@
+# Copyright (C) 2021 dihan official
+
+# This file is part of Sophia (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from bs4 import BeautifulSoup
 import urllib
-from Sophia import telethn as tbot
+from SophiaBot import telethn as tbot
 import glob
 import io
 import os
@@ -21,9 +39,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from Sophia import *
+from SophiaBot import *
 
-from Sophia.events import register
+from SophiaBot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
