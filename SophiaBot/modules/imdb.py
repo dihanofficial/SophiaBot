3d00700777b951692faa751594b8a84722708e96
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 16:50:22 +0530

    Update and rename __imdb.py to imdb.py

--- SophiaBot/modules/imdb.py
+++ SophiaBot/modules/imdb.py
@@ -1,11 +1,29 @@
-from Sophia import telethn as tbot
+# Copyright (C) 2021 dihan official
+
+# This file is part of Sophia (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from SophiaBot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from Sophia.events import register
+from SophiaBot.events import register
 
 langi = "en"
 
