0d3d110dd421e036e8f6852e411652e6c270cc3e
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:32:11 +0530

    Update locks_sql.py

--- SophiaBot/modules/sql/locks_sql.py
+++ SophiaBot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Sophia.modules.sql import SESSION, BASE
+from SophiaBot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
