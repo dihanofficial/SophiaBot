5d42c5f59d496338a20be0bc0c8840155f0a6565
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-29 14:17:12 +0530

    Update blacklist_sql.py

--- SophiaBot/modules/sql/blacklist_sql.py
+++ SophiaBot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from Sophia.modules.sql import SESSION, BASE
+from SophiaBot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
