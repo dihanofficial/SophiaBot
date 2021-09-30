26c4f4d82fbb6df316c7ef3d90062624816148f0
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-30 20:10:35 +0530

    Update and rename __weather.py to weather.py

--- SophiaBot/modules/weather.py
+++ SophiaBot/modules/weather.py
@@ -1,4 +1,4 @@
-from Sophia import telethn as tbot
+from SophiaBot import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from Sophia import *
+from SophiaBot import *
 
-from Sophia.events import register
+from SophiaBot.events import register
 
 
 async def is_register_admin(chat, user):
