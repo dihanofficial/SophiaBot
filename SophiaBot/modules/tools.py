e3e7a37d7e3f99e758d4fc367187aa7f843577c5
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-30 20:07:55 +0530

    Update and rename __tools.py to tools.py

--- SophiaBot/modules/tools.py
+++ SophiaBot/modules/tools.py
@@ -20,5 +20,5 @@ __help__ = """
  - /zip*:* reply to a telegram file to compress it in .zip format
  - /unzip*:* reply to a telegram file to decompress it from the .zip format
 
-*Powerted By* @DihanOfficial
+*Powerted By* @SophiaUpdates
 """
