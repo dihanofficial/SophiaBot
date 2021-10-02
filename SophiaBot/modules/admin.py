f98beeecfc58940daa0cf4821e9b3948381fda9a
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-02 10:18:55 +0530

    Update admin.py

--- SophiaBot/modules/admin.py
+++ SophiaBot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from Sophia import DRAGONS, dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler
-from Sophia.modules.helper_funcs.chat_status import (
+from SophiaBot import DRAGONS, dispatcher
+from SophiaBot.modules.disable import DisableAbleCommandHandler
+from SophiaBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from Sophia.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from Sophia.helper_extra.admin_rights import (
+from SophiaBot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from Sophia.modules.helper_funcs.extraction import (
+from SophiaBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Sophia.modules.log_channel import loggable
-from Sophia.modules.helper_funcs.alternate import send_message
-from Sophia.modules.helper_funcs.alternate import typing_action
+from SophiaBot.modules.log_channel import loggable
+from SophiaBot.modules.helper_funcs.alternate import send_message
+from SophiaBot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
