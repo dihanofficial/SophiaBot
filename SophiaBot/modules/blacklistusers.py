4ca4e9744b7aaba29fb405ef225a8f7d4f86130d
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-03 13:59:27 +0530

    Update blacklistusers.py

--- SophiaBot/modules/blacklistusers.py
+++ SophiaBot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import Sophia.modules.sql.blacklistusers_sql as sql
-from Sophia import (
+import SophiaBot.modules.sql.blacklistusers_sql as sql
+from SophiaBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from Sophia import (
     WOLVES,
     dispatcher,
 )
-from Sophia.modules.helper_funcs.chat_status import dev_plus
-from Sophia.modules.helper_funcs.extraction import (
+from SophiaBot.modules.helper_funcs.chat_status import dev_plus
+from SophiaBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Sophia.modules.log_channel import gloggable
+from SophiaBot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
