6d58c68a93a8ef2ba135bf117a6018e06af36fc8
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-03 18:38:29 +0530

    Update connection.py

--- SophiaBot/modules/connection.py
+++ SophiaBot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import Sophia.modules.sql.connection_sql as sql
-from Sophia import dispatcher, DRAGONS, DEV_USERS
-from Sophia.modules.helper_funcs import chat_status
-from Sophia.modules.helper_funcs.alternate import send_message, typing_action
+import SophiaBot.modules.sql.connection_sql as sql
+from SophiaBot import dispatcher, DRAGONS, DEV_USERS
+from SophiaBot.modules.helper_funcs import chat_status
+from SophiaBot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
