e1a4a23056787b7e8ad0602340b0de02e32ef018
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-04 06:27:14 +0530

    Update eval.py

--- SophiaBot/modules/eval.py
+++ SophiaBot/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from Sophia import LOGGER, dispatcher
-from Sophia.modules.helper_funcs.chat_status import dev_plus
+from SophiaBot import LOGGER, dispatcher
+from SophiaBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -76,7 +76,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "Sophia/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "SophiaBot/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
