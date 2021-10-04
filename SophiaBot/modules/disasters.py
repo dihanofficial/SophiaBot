170a8a19a2c07e22f3904cbee01e97222bdf45e8
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-04 06:22:03 +0530

    Update disasters.py

--- SophiaBot/modules/disasters.py
+++ SophiaBot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from Sophia import (
+from SophiaBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from Sophia import (
     WOLVES,
     dispatcher,
 )
-from Sophia.modules.helper_funcs.chat_status import (
+from SophiaBot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from Sophia.modules.helper_funcs.extraction import extract_user
-from Sophia.modules.log_channel import gloggable
+from SophiaBot.modules.helper_funcs.extraction import extract_user
+from SophiaBot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
