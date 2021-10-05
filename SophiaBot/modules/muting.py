2a952b4ef441f222899cee12b8b37fc75c8ab7c6
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-05 07:40:13 +0530

    Update muting.py

--- SophiaBot/modules/muting.py
+++ SophiaBot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from Sophia import LOGGER, TIGERS, dispatcher
-from Sophia.modules.helper_funcs.chat_status import (
+from SophiaBot import LOGGER, TIGERS, dispatcher
+from SophiaBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from Sophia.modules.helper_funcs.extraction import (
+from SophiaBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Sophia.modules.helper_funcs.string_handling import extract_time
-from Sophia.modules.log_channel import loggable
+from SophiaBot.modules.helper_funcs.string_handling import extract_time
+from SophiaBot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
