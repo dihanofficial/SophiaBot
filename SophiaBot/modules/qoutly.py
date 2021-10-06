986276006cbb89c0c36c9a6b307eedfc7b888ebe
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-06 14:05:11 +0530

    Update qoutly.py

--- SophiaBot/modules/qoutly.py
+++ SophiaBot/modules/qoutly.py
@@ -9,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from Sophia.events import register
+from SophiaBot.events import register
 
 COLORS = [
     "#F07975",
