d2e1d4d8ce790161bdd6f7a9d698102db03aee68
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-06 14:17:15 +0530

    Update speed_test.py

--- SophiaBot/modules/speed_test.py
+++ SophiaBot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from Sophia import DEV_USERS, dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler
-from Sophia.modules.helper_funcs.chat_status import dev_plus
+from SophiaBot import DEV_USERS, dispatcher
+from SophiaBot.modules.disable import DisableAbleCommandHandler
+from SophiaBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
