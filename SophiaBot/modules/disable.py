8cc1d9efc2547a5be68e9119b5dd6135f1d4e432
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-06 15:48:20 +0530

    Update disable.py

--- SophiaBot/modules/disable.py
+++ SophiaBot/modules/disable.py
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from Sophia.modules.helper_funcs.chat_status import (
+    from SophiaBot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from Sophia.modules.sql import disable_sql as sql
+    from SophiaBot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
