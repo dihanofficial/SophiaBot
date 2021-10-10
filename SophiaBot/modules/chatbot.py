7666ae6a9ceda198ca79657f30c298493d1535e0
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-10 13:20:12 +0530

    Sophia

---
+++ SophiaBot/modules/chatbot.py
@@ -0,0 +1,15 @@
+__help__ = """
+<b> Chatbot </b>
+Sophia AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+ - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
+ - /chatbot EN : Enables English only chatbot
+ 
+ 
+<b> Assistant </b>
+ - /ask [question]: Ask question from daisy
+ - /ask [reply to voice note]: Get voice reply
+ 
+
+"""
+
+__mod_name__ = "AI Assistant"
