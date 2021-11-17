330ad9d321e023a97dc39b59ea55d7f1f29298a0
thunderteampromax <94462160+thunderteampromax@users.noreply.github.com>
2021-11-17 13:31:40 +0530

    Update google.py

--- SophiaBot/modules/google.py
+++ SophiaBot/modules/google.py
@@ -285,7 +285,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n »»» App By @SophiaSLBot «««« "
+        app_details += "\n\n »»» App By @thunder_gohelp_bot «««« "
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -304,6 +304,6 @@ __help__ = """
  - /github <username>*:* Get information about a GitHub user.
  - /country <country name>*:* Gathering info about given country
  - /imdb <Movie name>*:* Get full info about a movie with imdb.com
- - Sophia <query>*:* Sophia answers the query
-    - Ex: `Sophia where is Sri Lanka ?`
+ - Thunder GoHelp <query>*:* Thunder GoHelp answers the query
+    - Ex: `Thunder GoHelp where is Sri Lanka ?`
 """
